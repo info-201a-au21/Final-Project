b0f1f399bd2b99c2dc4de9bfa05feb5fb0235b14
FrancesZXY <zhangxinyanxy@163.com>
2021-11-22 14:07:04 -0800

    update

--- Exploratory Analysis .R
+++ Exploratory Analysis .R
@@ -1,1 +1,5 @@
 
+atlantic <- read.csv("https://raw.githubusercontent.com/info-201a-au21/Final-Project/main/dataset/atlantic.csv?token=AV3GEWVSSS2O3HFAS5IN7MLBUVEYW")
+earthquake <- read.csv("https://raw.githubusercontent.com/info-201a-au21/Final-Project/main/dataset/database.csv?token=AV3GEWWUKHAHG62JUEDG2MLBUVE2G")
+severe_weather <- read.csv("https://raw.githubusercontent.com/info-201a-au21/Final-Project/main/dataset/severe_weather.csv?token=AV3GEWTQ7BYVKLUBOKUZDRLBUVE3K")
+pacific <- read.csv("https://raw.githubusercontent.com/info-201a-au21/Final-Project/main/dataset/pacific.csv?token=AV3GEWSHDW2UWVZXBI52QATBUVE4S")
