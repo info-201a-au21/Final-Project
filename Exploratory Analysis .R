a136839e974dd75c14af6a98c2f1a61f9e92821a
FrancesZXY <zhangxinyanxy@163.com>
2021-11-22 17:18:10 -0800

    group analysis

--- Exploratory Analysis .R
+++ Exploratory Analysis .R
@@ -2,77 +2,13 @@ atlantic <- read.csv("https://raw.githubusercontent.com/info-201a-au21/Final-Pro
 earthquake <- read.csv("https://raw.githubusercontent.com/info-201a-au21/Final-Project/main/dataset/database.csv?token=AV3GEWWUKHAHG62JUEDG2MLBUVE2G")
 severe_weather <- read.csv("https://raw.githubusercontent.com/info-201a-au21/Final-Project/main/dataset/severe_weather.csv?token=AV3GEWTQ7BYVKLUBOKUZDRLBUVE3K")
 pacific <- read.csv("https://raw.githubusercontent.com/info-201a-au21/Final-Project/main/dataset/pacific.csv?token=AV3GEWSHDW2UWVZXBI52QATBUVE4S")
+weather <- read.csv("~/Desktop/WeatherEvents_Jan2016-Dec2020.csv", stringsAsFactors = FALSE)
 
 #Variable
 
 
-
-
-
-#Hurricane Chart
-library("ggplot2")
-library("dplyr")
-library("stringr")
-# devtools::install_github("renkun-ken/formattable")
-library(formattable)
-
-pacific <- pacific %>%
-  select(ID, Name, Date, Latitude, Longitude, Maximum.Wind)
-atlantic <- atlantic %>%
-  select(ID, Name, Date, Latitude, Longitude, Maximum.Wind)
-
-
-pacific_time <- pacific %>%
-  group_by(ID) %>%
-  summarise(time_span = max(Date)- min(Date)) 
-pacific_max <- pacific %>%
-  group_by(ID) %>%
-  summarise(Max_wind = mean(Maximum.Wind))
-pacific_date <- pacific %>%
-  group_by(ID) %>%
-  summarise(Date = mean(Date))
-pacific_date$Date <- pacific_date$Date / 10000
-pacific_date$Date <- formattable(pacific_date$Date, digits = 4, format = "f")
-
-pacific_table <- pacific_time %>%
-  left_join(pacific_max, by = "ID") %>%
-  left_join(pacific_date, by = "ID")
-
-(chart_pacific <- ggplot(data = pacific_table) +
-    geom_point(mapping = aes(x = Date, y = time_span, color = Max_wind)) +
-    labs(x = "Date", y = "Time Span (Days)", 
-         title = "Time Span of Pacific Hurricane from 1949 to 2015"))
-
-atlantic_time <- atlantic %>%
-  group_by(ID) %>%
-  summarise(time_span = max(Date)- min(Date)) 
-atlantic_max <- atlantic %>%
-  group_by(ID) %>%
-  summarise(Max_wind = mean(Maximum.Wind))
-atlantic_date <- atlantic %>%
-  group_by(ID) %>%
-  summarise(Date = mean(Date))
-atlantic_date$Date <- atlantic_date$Date / 10000
-atlantic_date$Date <- formattable(atlantic_date$Date, digits = 4, format = "f")
-
-atlantic_table <- atlantic_time %>%
-  left_join(atlantic_max, by = "ID") %>%
-  left_join(atlantic_date, by = "ID") %>%
-  filter(time_span <= "1000") %>%
-  filter(Max_wind >= "0")
-
-(chart_atlantic <- ggplot(data = atlantic_table) +
-    geom_point(mapping = aes(x = Date, y = time_span, color = Max_wind)) +
-    labs(x = "Date", y = "Time Span (Days)", 
-         title = "Time Span of Atlantic Hurricane from 1851 to 2013"))
-
-#Severe Weather Chart
-library(tidyverse)
-library("dplyr")
-library("ggplot2")
-library("lubridate")
-install.packages("lubridate")
-# Filter the "severe" level weather events
+# Variable1: 
+#Filter the "severe" level weather events
 weather_data <- weather %>%
   filter(Severity == "Severe") %>%
   select(Type, StartTime.UTC., EndTime.UTC.)
@@ -88,12 +24,86 @@ weather_time_span <- weather_data %>%
   group_by(Type) %>%
   summarise(mean_time_span = mean(time_span))
 
-# Severe Weather Type vs. Mean Time Span Chart
-severe_weather_chart <- ggplot(data = weather_time_span) + 
-  geom_col(mapping = aes(x = Type, y = mean_time_span)) + 
-  labs(
-    title = "Severe Weather Mean Time Span in U.S. (2016-2020)",
-    x = "Severe Weather Type",
-    y = "Mean Time Span (mins)"
-  )
+# Variable2: cold_varibale
+cold_varibale <- weather_time_span %>% 
+  filter( mean_time_span == max(mean_time_span)) %>% 
+  pull(mean_time_span)
+cold_varibale
 
+
+
+
+#Variable3: state sum
+
+weather_data <- weather %>%
+  filter(Severity == "Severe") %>%
+  group_by(State) %>%
+  summarise(num = length(Severity))
+
+max_state_name <- weather_data %>% 
+  filter(num == max(num)) %>% 
+  pull(State)
+
+max_state_num <- weather_data %>% 
+  filter(num == max(num)) %>% 
+  pull(num)
+
+
+
+#Variable 4: high_year_disaster_hurricane_earthquake
+high_year_disaster_hurricane_earthquake <- merge_disaster %>%
+  group_by(year) %>%
+  summarise(total_disater = earthquake_num + atlantic_num + pacific_num) %>%
+  summarise(total_disater = max(total_disater))
+
+#Variable 5: high_year_change
+year_change <- merge_disaster %>%
+  summarise(earthquake_change = earthquake_num - lag(earthquake_num, default = T), 
+            atlantic_change = atlantic_num - lag(atlantic_num, default = T), 
+            pacific_change = pacific_num - lag(pacific_num, default = T))
+
+year_change_earthquake <- year_change %>%
+  filter(earthquake_change > 0) %>%
+  summarise(type = "earthquake", num = n()) 
+year_change_atlantic <- year_change %>%
+  filter(atlantic_change > 0) %>%
+  summarise(type = "atlantic", num = n()) 
+year_change_pacific <- year_change %>%
+  filter(pacific_change > 0) %>%
+  summarise(type = "pacific", num = n())
+
+compare <- year_change_earthquake %>%
+  full_join(year_change_atlantic) %>%
+  full_join(year_change_pacific)
+
+high_year_change <- compare %>%
+  filter(num == max(num)) %>%
+  pull(type)
+
+#Variable 6: mean_atlantic_time 
+pacific_time <- pacific %>%
+  group_by(ID) %>%
+  summarise(time_span = max(Date)- min(Date)) 
+mean_pacific_time <- pacific_table %>%
+  summarise(mean = mean(time_span))
+mean_pacific_time <- select(mean_pacific_time, mean)
+mean_pacific_time <- pull(mean_pacific_time)
+atlantic_time <- atlantic %>%
+  group_by(ID) %>%
+  summarise(time_span = max(Date)- min(Date)) 
+mean_atlantic_time <- atlantic_table %>%
+  summarise(mean = mean(time_span))
+mean_atlantic_time <- select(mean_atlantic_time, mean)
+mean_atlantic_time <- pull(mean_atlantic_time)
+
+
+
+
+
+
+
+
+
+
+  
+  
